87957667da56f72d92ffe65e575976bd7e9f2816
kanchan-solanke <kanchanssolanke@gmail.com>
2021-08-02 21:28:26 +0530

    check valid mobile number

--- UserRegistrationCase.java
+++ UserRegistrationCase.java
@@ -3,13 +3,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class UserRegistrationCase {
 
-    public boolean validateEmailTest(String emailTest){
-        boolean check = Pattern.matches("abc.xyz@bl.co.in" ,emailTest);
+    public boolean validateEmailTest(String emailTest) {
+        boolean check = Pattern.matches("abc.xyz@bl.co.in", emailTest);
         return check;
     }
 
     //for First Name
-  public boolean validateFirstName(String str) {
+    public boolean validateFirstName(String str) {
         boolean check = Pattern.matches("^[A-Z]{1}+[a-z]{3,}$", str);
         return check;
     }
@@ -19,4 +19,12 @@ public class UserRegistrationCase {
         boolean check = Pattern.matches("^[A-Z]{1}+[a-z]{3,}$", str);
         return check;
     }
+
+    //for Mobile number
+    public boolean validateMobileNumber(String str) {
+        boolean check = Pattern.matches("^([0-9]{2}) ([1-9]{1}[0-9]{9})$" , str);
+        return check;
+    }
+
+    
 }
--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -17,4 +17,11 @@ public class UserRegistrationTest {
         boolean result = userRegistration.validateLastName("Solanke");
         Assert.assertEquals(true, result);
     }
+@Test
+    public void givenMobilenoconditionmeetthenTrue(){
+    UserRegistrationCase userRegistration = new UserRegistrationCase();
+    boolean result = userRegistration.validateMobileNumber("91 9919819801");
+    Assert.assertEquals(true, result);
+
+}
 }
